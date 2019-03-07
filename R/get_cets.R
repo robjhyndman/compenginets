19528b0c36f989c4753afc9de840206f7e1944c8
Fin Yang <YangYangzhuoran@gmail.com>
2019-03-07 13:40:06 +1100

    roxygened

--- R/get_cets.R
+++ R/get_cets.R
@@ -1,15 +1,44 @@
 
 #' Extracting time series from the CompEngine database
 #'
+#' Return a time series with a specified name or a list of time series in a specified category.
+#'
+#' @param key The name of the time series or the name of the category,
+#' depending on value of \code{category}
+#' @param category If \code{TRUE}, the \code{get_cets}
+#' will return time series whose category match the keyword or belong to
+#' some subcategory under the matched category. If \code{FALSE},
+#' the function will return time series with the name of \code{key}.
+#' @param maxpage The maximum number of pages to extract.
+#' Due to the number of time series that can be returned, a maximum of 10 time series are returned per page.
+#' The default is 10 pages (100 time series). This argument is only valid when \code{category} is set to \code{TRUE}.
+#' @return A list consisting of the selected series with class \code{ts}.
+#'
+#' NOTE: Due to the large variation in the properties of time series stored in the CompEngine database,
+#' the \code{tsp} attribute (the start time in time units, the end time and the frequency) is not tailor made for each time series.
+#' If needed, user can customize the \code{tsp} attribute based on the output attribute of \code{samplingInformation}.
+#'
+#' Some of the attributes are listed as follow:
+#' \item{name}{The name of the time series}
+#' \item{description}{the description of the time series}
+#' \item{samplingInformation}{Indicating the frequency and other sampling related information}
+#' \item{tags}{The tags in the database}
+#' \item{category}{The name of the category and the hierarchy of the current category. Seealso \code{\link{category_scraping}}.}
+#' \item{sfi}{"special feature identification" information}
+#' \item{source}{The source of the timeseries}
+#' \item{tsp}{Tsp Attribute of Time-Series-like Objects. See \code{\link{tsp}}}
 #'
 #'
 #'
-#'
-#'
-#'
+#' @author Yangzhuoran Yang
 #' @examples
-#' finance_m4 <- get_cets("M4_W138_Finance_1", category = FALSE)
-#' a <- get_cets("real")
+#' # Getting series within Finance category (including subcategory)
+#' cets_finance <- get_cets("finance")
+#' unique(mapply(attr, cets_finance, MoreArgs = list(which = "category.name")))
+#' # Getting series by its name
+#' W138_finance_m4 <- get_cets("M4_W138_Finance_1", category = FALSE)
+#' @seealso \code{\link{category_scraping}}
+#' @export
 get_cets <- function(key, category = TRUE, maxpage = 10){
   URL <- "https://www.comp-engine.org"
 
@@ -17,8 +46,9 @@ get_cets <- function(key, category = TRUE, maxpage = 10){
     PATH <- paste0("api/public/timeseries/filter?format=json&category=",key,"&page=",1)
     content <- access_api(url = URL, path = PATH)
     content_list <- get_datalist(content)
-    if(content$totalPages > 1 & maxpage !=1){
-      content_rest <- lapply(2:min(maxpage, content$totalPages), category_rest, key = key)
+    if(content$totalPages > 1 & maxpage > 1){
+      content_rest <- lapply(2:min(maxpage, content$totalPages),
+                             category_rest, key = key, URL = URL, PATH = PATH)
     }
     rest_list <- do.call(c,lapply(content_rest, get_datalist))
 
@@ -53,7 +83,7 @@ access_api <- function(url, path){
   return(content)
 }
 
-category_rest <- function(key, page){
+category_rest <- function(key, page, URL, PATH){
   PATH <- paste0("api/public/timeseries/filter?format=json&category=",key,"&page=",page)
   access_api(url = URL, path = PATH)
 }
