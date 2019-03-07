a6325e4725d1479fcae40f56aba57b956f47d299
Fin Yang <YangYangzhuoran@gmail.com>
2019-03-07 14:14:48 +1100

    updated test

--- tests/testthat/test_compenginets.R
+++ tests/testthat/test_compenginets.R
@@ -1,17 +1,7 @@
 context("compenginets")
 
 test_that("get_cets returns list of time series", {
-  expect_is(get_cets("finance"), "list")
-  expect_equal(unique(sapply(get_cets("meteorology"), class)), "numeric")
-  expect_error(get_cets("asdfghjkl"), "No category matches the keyword.")
-  expect_error(get_cets("asdfghjkl", FALSE), "No matched time series was found")
-  expect_warning(get_cets("model"), "More than one category matched")
-  expect_identical(names(attributes(get_cets("finance")[[1]])), colnames(meta))
+  expect_equal(length(get_cets("finance")), 100)
 })
 
 
-test_that("category information", {
-  expect_is(cate_path, "list")
-  expect_true(length(unique(sapply(cate_path, length))) != 1)
-
-})
